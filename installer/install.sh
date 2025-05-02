2175d70f43dcae9ea143f261b3c18c0be4b82d4f
Frideru <zykinf@gmail.com>
2025-05-02 16:04:31 +0500

    change structure and add auto installer programs with README

---
+++ installer/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# NOTE: First things first: 
+# $ su root 
+# $ install sudo git
+# $ sudo usermod -aG sudo $USER
+# $ git clone https://github.com/Frideru/system_config.git
+
+# Стандартный набор программ для установки
+bash ./system_utilites
+
+# Дополнительный набор программ
+bash ./system_other
+
+# Удаление мусора
+sudo apt autoremove
+
+# Настройка конфигурации
+bash ./system_configuration
+
