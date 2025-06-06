79b47a9e68f03e0b595616ef842401e87843ae6e
Frideru <zykinf@gmail.com>
2025-06-06 15:52:54 +0500

    added Magit installation

--- init.el
+++ init.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(centaur-tabs company vscode-dark-plus-theme use-package)))
+ '(package-selected-packages
+   '(magit centaur-tabs company vscode-dark-plus-theme use-package)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -60,3 +61,7 @@
 ;; Настройки company-mode
 (setq company-minimum-prefix-length 1) ; Минимальная длина префикса для автодополнения
 (setq company-idle-delay 0.3) ; Задержка перед показом подсказок
+
+;; Git | It's Magit
+;; M-x package-refresh-contents RET
+;; M-x package-install RET magit RET
