1d37b183448a50fdc803842c756a6ec9b9ce852a
Frideru <zykinf@gmail.com>
2025-10-17 16:30:32 +0500

    autopush from script

--- installer/install.sh
+++ installer/install.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-# NOTE: First things first: 
-# $ su root 
+# First things first: 
+# $ su root
 # $ install sudo git
 # $ sudo usermod -aG sudo $USER
 # $ git clone https://github.com/Frideru/system_config.git
 
+MAINUSER=$(cat /etc/passwd | grep 1000 | awk -F: '{print $1}')
+
 # Стандартный набор программ для установки
-bash ./system_utilites
+# chromium
+sudo apt install -y xorg i3 i3blocks lightdm terminator network-manager pulseaudio pavucontrol firefox-esr pkexec polkitd lxpolkit curl gcc xclip feh mc htop rofi kbdd
 
 # Дополнительный набор программ
-bash ./system_other
+# mutt picom conky lxappearance emacs
+sudo apt install -y arandr network-manager-gnome flameshot vlc blueman mousepad evince keepassxc ncdu nemo gnome-disk-utility
 
 # Удаление мусора
 sudo apt clean
 sudo apt autoremove
 
-# Настройка конфигурации
-bash ./system_configuration
+# Переходим с рута на пользователя системы
+su $MAINUSER
 
+# Создание папок
+mkdir -p $HOME/.programs
+mkdir -p $HOME/.scripts
+mkdir -p $HOME/.fonts
+mkdir -p $HOME/.config/terminator
+
+# Установка шрифтов
+cp $HOME/system_config/fonts/Hack/* $HOME/.fonts/
+cp $HOME/system_config/fonts/JetBrainsMono/* $HOME/.fonts/
+fc-cache -f -v
+
+cp $HOME/system_config/configs/terminator/config $HOME/.config/terminator/
+cp $HOME/system_config/configs/i3wm/* $HOME/.config/i3/
+cp $HOME/system_config/configs/i3blocks/ $HOME/.config/
+sudo cp $HOME/system_config/rofi-power-menu/rofi-power-menu/rofi-power-menu /usr/bin/
