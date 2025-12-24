3d49b10daf87aff7b0e155a84638b7eef2ca239e
Frideru <zykinf@gmail.com>
2025-12-24 15:14:17 +0500

    autopush from script

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -5,6 +5,9 @@ config.font_size = 10
 config.color_scheme = 'Apple System Colors'
 
 config.keys = {
+  --
+  -- Разделение панели
+  -- 
   -- Разделить панель ПО ВЕРТИКАЛИ (новая панель будет снизу)
   {
     key = 'x', 
@@ -17,7 +20,11 @@ config.keys = {
     mods = 'ALT', 
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
   },
-    -- Перемещение влево
+
+  --
+  -- Перемещение фокуса
+  -- 
+  -- Перемещение влево
   {
     key = 'LeftArrow',
     mods = 'ALT',
@@ -40,7 +47,50 @@ config.keys = {
     key = 'DownArrow',
     mods = 'ALT',
     action = wezterm.action.ActivatePaneDirection 'Down',
-  }
+  },
+
+  --
+  -- Перемещение окон
+  --
+  -- ПЕРЕМЕЩЕНИЕ (смена мест) панелей
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.RotatePanes 'CounterClockwise',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.RotatePanes 'Clockwise',
+  },
+
+  --
+  -- Изменение размера окон
+  -- 
+  -- Изменить размер ВЛЕВО
+  {
+    key = 'LeftArrow',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Left', 5 },
+  },
+  -- Изменить размер ВПРАВО
+  {
+    key = 'RightArrow',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Right', 5 },
+  },
+  -- Изменить размер ВВЕРХ
+  {
+    key = 'UpArrow',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Up', 5 },
+  },
+  -- Изменить размер ВНИЗ
+  {
+    key = 'DownArrow',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Down', 5 },
+  },
 }
 
 return config
