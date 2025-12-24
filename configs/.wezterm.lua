de1c379c1639924b890904906e7250b14172bb75
Frideru <zykinf@gmail.com>
2025-12-24 14:47:20 +0500

    autopush from script

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -4,4 +4,43 @@ local config = wezterm.config_builder()
 config.font_size = 10
 config.color_scheme = 'Apple System Colors'
 
+config.keys = {
+  -- Разделить панель ПО ВЕРТИКАЛИ (новая панель будет снизу)
+  {
+    key = 'x', 
+    mods = 'ALT', 
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  -- Разделить панель ПО ГОРИЗОНТАЛИ (новая панель будет справа)
+  {
+    key = 'c', 
+    mods = 'ALT', 
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  },
+    -- Перемещение влево
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  -- Перемещение вправо
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  -- Перемещение вверх
+  {
+    key = 'UpArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  -- Перемещение вниз
+  {
+    key = 'DownArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  }
+}
+
 return config
