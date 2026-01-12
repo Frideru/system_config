c110d83016f32d9cae6970762b9f0318043bf278
Felix <100554404+Frideru@users.noreply.github.com>
2026-01-12 14:41:52 +0500

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -9,32 +9,27 @@
 MAINUSER=$(cat /etc/passwd | grep 1000 | awk -F: '{print $1}')
 
 # Стандартный набор программ для установки
-# chromium
-sudo apt install -y xorg i3 i3blocks lightdm terminator network-manager pulseaudio pavucontrol firefox-esr pkexec polkitd lxpolkit curl gcc xclip feh mc htop rofi kbdd
+# chromium terminator
+sudo apt install -y xorg i3 i3blocks lightdm alacritty network-manager pulseaudio pavucontrol firefox-esr pkexec polkitd lxpolkit curl gcc xclip feh mc htop rofi kbdd
 
 # Дополнительный набор программ
 # mutt picom conky lxappearance emacs
-sudo apt install -y arandr network-manager-gnome flameshot vlc blueman mousepad evince keepassxc ncdu nemo gnome-disk-utility
+sudo apt install -y arandr network-manager-gnome flameshot vlc blueman mousepad evince keepassxc ncdu nemo gnome-disk-utility tmux
 
 # Удаление мусора
 sudo apt clean
 sudo apt autoremove
 
-# Переходим с рута на пользователя системы
-su $MAINUSER
-
 # Создание папок
-mkdir -p $HOME/.programs
-mkdir -p $HOME/.scripts
-mkdir -p $HOME/.fonts
-mkdir -p $HOME/.config/terminator
+su $MAINUSER -с mkdir -p $HOME/.programs
+su $MAINUSER -с mkdir -p $HOME/.scripts
+su $MAINUSER -с mkdir -p $HOME/.fonts
 
 # Установка шрифтов
-cp $HOME/system_config/fonts/Hack/* $HOME/.fonts/
-cp $HOME/system_config/fonts/JetBrainsMono/* $HOME/.fonts/
-fc-cache -f -v
+su $MAINUSER -с cp $HOME/system_config/fonts/Hack/* $HOME/.fonts/
+su $MAINUSER -с cp $HOME/system_config/fonts/JetBrainsMono/* $HOME/.fonts/
+su $MAINUSER -с fc-cache -f -v
 
-cp $HOME/system_config/configs/terminator/config $HOME/.config/terminator/
-cp $HOME/system_config/configs/i3wm/* $HOME/.config/i3/
-cp $HOME/system_config/configs/i3blocks/ $HOME/.config/
-sudo cp $HOME/system_config/rofi-power-menu/rofi-power-menu/rofi-power-menu /usr/bin/
+su $MAINUSER -с cp $HOME/system_config/configs/i3wm/* $HOME/.config/i3/
+su $MAINUSER -с cp $HOME/system_config/configs/i3blocks/ $HOME/.config/
+cp /home/$MAINUSER/system_config/rofi-power-menu/rofi-power-menu/rofi-power-menu /usr/bin/
